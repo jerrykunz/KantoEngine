b3fdee044aa79aa8de772fc053e9cd8e9b60dc29
Jeremia Kunnari <jeremia@gmail.com>
2024-03-21 12:57:56 +0200

    small fixes

--- KantoEngine/src/Kanto/Platform/OpenGL/OpenGLRendererAPI.cpp
+++ KantoEngine/src/Kanto/Platform/OpenGL/OpenGLRendererAPI.cpp
@@ -1,5 +1,6 @@
 #include "knpch.h"
-#include "Platform/OpenGL/OpenGLRendererAPI.h"
+#include "Kanto/Platform/OpenGL/OpenGLRendererAPI.h"
+//#include "Platform/OpenGL/OpenGLRendererAPI.h"
 
 #include <glad/glad.h>
 
